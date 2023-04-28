fcae22171f96246876e035a06063c6412c8bd129
Tuomas Huhtala <tuomas.huhtala@icloud.com>
2023-04-28 18:17:01 +0300

    JPS

--- src/algorithms/jps.py
+++ src/algorithms/jps.py
@@ -1,107 +1,186 @@
 import math
 import heapq
 
-def jps(start, goal, grid):
+class JPS:
+    """
+    Jump Point Search algorithm
+    """
+    def __init__(self, matrix):
+        self.matrix = matrix
+        self.distances = [[float('inf')] * len(matrix[0]) for _ in range(len(matrix))]
+        self.visited = [[False] * len(matrix[0]) for _ in range(len(matrix))]
+        self.previous = [[None] * len(matrix[0]) for _ in range(len(matrix))]
+        self.operations = 0
 
-    def is_obstructed(x, y):
-        return x < 0 or y < 0 or x >= len(grid) or y >= len(grid[0]) or grid[x][y] == 1
+    def search(self, start, goal):
+        self.distances[start[0]][start[1]] = 0
+        self.previous[start[0]][start[1]] = start
 
-    def has_forced_neighbour(x, y):
-        for dx, dy in ((0, 1), (1, 0), (0, -1), (-1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)):
-            nx, ny = x + dx, y + dy
-            if not is_obstructed(nx, ny) and (dx, dy) != (0, 0)\
-                and (dx, dy) != (last_dir[x][y][0], last_dir[x][y][1]):
-                if (nx, ny) == goal or \
-                    is_obstructed(nx - last_dir[nx][ny][0], ny - last_dir[nx][ny][1]):
-                    return True
-        return False
+        heap = [(self.heuristic(start, goal), start)]
+        while heap:
+            self.operations += 1
+            
+            current = heapq.heappop(heap)[1]
+            if current == goal:
+                return self.get_path(start, goal), self.operations, self.visited
+            if not self.visited[current[0]][current[1]]:
+                self.visited[current[0]][current[1]] = True
+                neighbours = self.get_neighbours(current)
 
-    def direction(x, y):
-        dx, dy = y[0] - x[0], y[1] - x[1]
-        if dx > 0:
-            if dy > 0:
-                return (1, 1)
-            elif dy < 0:
-                return (1, -1)
-            else:
-                return (1, 0)
-        elif dx < 0:
-            if dy > 0:
-                return (-1, 1)
-            elif dy < 0:
-                return (-1, -1)
-            else:
-                return (-1, 0)
-        else:
-            if dy > 0:
-                return (0, 1)
-            else:
-                return (0, -1)
+                for neighbour in neighbours:
+                    direction = self.get_direction(current, neighbour)
+                    jump_point = self.jump(neighbour, direction, goal)
 
-    def prune(x, neighbours):
-        pruned = []
-        last_dir[x[0]][x[1]] = (0, 0)
-        for y in neighbours:
-            di = direction(x, y)
-            if di != last_dir[x[0]][x[1]]:
-                if is_obstructed(x[0] + di[0], x[1] + di[1]):
-                    continue
-                if di[0] != 0 and di[1] != 0 and (is_obstructed(x[0], x[1] + di[1]) \
-                    or is_obstructed(x[0] + di[0], x[1])):
-                    continue
-                pruned.append(y)
-            last_dir[y[0]][y[1]] = di
-        return pruned
+                    if jump_point is None:
+                        continue
 
-    def jump(x, d, s, g):
-        n = (x[0] + d[0], x[1] + d[1])
-        if is_obstructed(n[0], n[1]):
+                    old_distance = self.distances[jump_point[0]][jump_point[1]]
+                    new_distance = self.distances[current[0]][current[1]] + self.get_jump_length(current, jump_point, direction)
+
+                    if new_distance < old_distance:
+                        self.distances[jump_point[0]][jump_point[1]] = new_distance
+                        self.previous[jump_point[0]][jump_point[1]] = current
+                        heapq.heappush(heap, (self.heuristic(jump_point, goal) + new_distance, jump_point))
+
+        return None
+
+    def jump(self, current, direction, goal):
+        next_point =  (current[0] + direction[0], current[1] + direction[1])
+
+        if not self.is_valid(next_point):
             return None
-        if n == g:
-            return n
-        if has_forced_neighbour(n[0], n[1]):
-            return n
-        if d in [(1,1),(1,-1),(-1,1),(-1,-1)]:
-            for di in [(1,0),(0,1),(-1,0),(0,-1)]:
-                if jump(n, di, s, g) is not None:
-                    return n
-        return jump(n, d, s, g)
 
-    def neighbours(x):
-        n = []
-        for dx, dy in ((0, 1), (1, 0), (0, -1), (-1, 0)):
-            nx, ny = x[0] + dx, x[1] + dy
-            if not is_obstructed(nx, ny):
-                n.append((nx, ny))
-        return n
+        if next_point == goal:
+            return next_point
 
-    def reconstruct_path(came_from, current, operations):
-        path = [current]
-        while current in came_from:
-            current = came_from[current]
+        y = next_point[0]
+        x = next_point[1]
+
+        if direction[0] == 0:
+            if not self.is_valid((y + 1, current[0])) and self.is_valid((y + 1, x))\
+            or not self.is_valid((y - 1, current[0])) and self.is_valid((y - 1, x)):
+                return next_point
+            
+        elif direction[1] == 0:
+            if not self.is_valid((current[0], x + 1)) and self.is_valid((y, x + 1))\
+            or not self.is_valid((current[0], x - 1)) and self.is_valid((y, x - 1)):
+                return next_point
+            
+        if direction[1] != 0 and direction[0] != 0:
+            v_jump = self.jump(next_point, (0, direction[1]), goal)
+            if v_jump is not None:
+                return next_point
+            
+            h_jump = self.jump(next_point, (direction[0], 0), goal)
+            if h_jump is not None:
+                return next_point
+            
+        return self.jump(next_point, direction, goal)
+    
+    def get_neighbours(self, current):
+        neighbours = []
+        y, x = current
+
+        prev = self.previous[y][x]
+
+        if prev is not None or prev is None:
+            for dy in range(-1, 2):
+                for dx in range(-1, 2):
+                    if dx == 0 and dy == 0:
+                        continue
+                    neighbour_y, neighbour_x = y + dy, x + dx
+                    if self.is_valid((neighbour_y, neighbour_x)):
+                        neighbours.append((neighbour_y, neighbour_x))
+                    else:
+                        if dx == 0:
+                            if self.is_valid((neighbour_y, neighbour_x + 1)):
+                                neighbours.append((neighbour_y, neighbour_x + 1))
+                            if self.is_valid((neighbour_y, neighbour_x - 1)):
+                                neighbours.append((neighbour_y, neighbour_x - 1))
+                        elif dy == 0:
+                            if self.is_valid((neighbour_y + 1, neighbour_x)):
+                                neighbours.append((neighbour_y + 1, neighbour_x))
+                            if self.is_valid((neighbour_y - 1, neighbour_x)):
+                                neighbours.append((neighbour_y - 1, neighbour_x))
+            return neighbours
+        
+        direction = self.get_direction(prev, current)
+        dy, dx = direction[0], direction[1]
+
+        if dx == 0:
+            if self.is_valid((y + dy, x)):
+                neighbours.append((y + dy, x))
+            if not self.is_valid((y - dy, x - 1)):
+                if self.is_valid((y, x - 1)):
+                    neighbours.append((y, x - 1))
+                    if self.is_valid((y + dy, x)) and self.is_valid((y + dy, x - 1)):
+                        neighbours.append((y + dy, x - 1))
+            if not self.is_valid((y - dy, x + 1)):
+                if self.is_valid((y, x + 1)):
+                    neighbours.append((y, x + 1))
+                    if self.is_valid((y + dy, x)) and self.is_valid((y + dy, x + 1)):
+                        neighbours.append((y + dy, x + 1))
+        elif dy == 0:
+            if self.is_valid((y, x + dx)):
+                neighbours.append((y, x + dx))
+            if not self.is_valid((y - 1, x - dx)):
+                if self.is_valid((y - 1, x)):
+                    neighbours.append((y - 1, x))
+                    if self.is_valid((y, x + dx)) and self.is_valid((y - 1, x + dx)):
+                        neighbours.append((y - 1, x + dx))
+            if not self.is_valid((y + 1, x - dx)):
+                if self.is_valid((y + 1, x)):
+                    neighbours.append((y + 1, x))
+                    if self.is_valid((y, x + dx)) and self.is_valid((y + 1, x + dx)):
+                        neighbours.append((y + 1, x + dx))
+        else:
+            if self.is_valid((y + dy, x + dx)):
+                neighbours.append((y + dy, x + dx))
+            if not self.is_valid((y, x + dx)):
+                if self.is_valid((y - dy, x)):
+                    neighbours.append((y - dy, x))
+                    if self.is_valid((y - dy, x + dx)) and self.is_valid((y - dy, x)):
+                        neighbours.append((y - dy, x + dx))
+                if self.is_valid((y + dy, x)):
+                    neighbours.append((y + dy, x))
+                    if self.is_valid((y + dy, x + dx)) and self.is_valid((y + dy, x)):
+                        neighbours.append((y + dy, x + dx))
+
+        return neighbours
+
+
+    def get_direction(self, current, neighbour):
+        return (neighbour[0] - current[0], neighbour[1] - current[1])
+    
+    def get_distance(self, current, neighbour):
+        return math.sqrt((current[0] - neighbour[0])**2 + (current[1] - neighbour[1])**2)
+    
+    def is_valid(self, point):
+        return 0 <= point[0] < len(self.matrix) and 0 <= point[1] < len(self.matrix[0]) and self.matrix[point[0]][point[1]] == 0
+
+    def heuristic(self, current, goal):
+        return math.sqrt((current[0] - goal[0])**2 + (current[1] - goal[1])**2)
+    
+    def get_path(self, start, goal):
+        path = []
+        current = goal
+        while current != start:
             path.append(current)
-        return operations, path[::-1]
+            current = self.previous[current[0]][current[1]]
+        path.append(start)
+        return path[::-1]
 
-    last_dir = [[(0, 0) for _ in range(len(grid[0]))] for _ in range(len(grid))]
-    start, goal = (start[0], start[1]), (goal[0], goal[1])
+    def get_jump_length(self, current, neighbour, direction):
+        x = current[0] - neighbour[0]
+        y = current[1] - neighbour[1]
 
-    f_score = {start: math.sqrt((start[0] - goal[0])**2 + (start[1] - goal[1])**2)}
-    open_set = [(f_score[start], start)]
-    came_from = {}
-    g_score = {start: 0}
-    operations = 0
+        if x < 0:
+            x = -x
 
-    while open_set:
-        operations += 1
-        current = heapq.heappop(open_set)[1]
-        if current == goal:
-            return reconstruct_path(came_from, current, operations)
-        for n in prune(current, neighbours(current)):
-            g = g_score[current] + math.sqrt((n[0] - current[0])**2 + (n[1] - current[1])**2)
-            if n not in g_score or g < g_score[n]:
-                came_from[n] = current
-                g_score[n] = g
-                f_score[n] = g + math.sqrt((n[0] - goal[0])**2 + (n[1] - goal[1])**2)
-                if n not in [item[1] for item in open_set]:
-                    heapq.heappush(open_set, (f_score[n], n))
-    return None
+        if y < 0:
+            y = -y
+
+        if direction[0] == 0 or direction[1] == 0:
+            return x + y
+        else:
+            return x * (2)**0.5
--- src/main.py
+++ src/main.py
@@ -1,114 +1,51 @@
-import pygame
 from time import time
 from PIL import Image, ImageDraw
-from algorithms.dijkstra import Dijkstra
+#from algorithms.dijkstra import Dijkstra
 from algorithms.astar import AStar
-from algorithms.jps import jps
+from algorithms.jps import JPS
 
 def main():
-        # Load the maze image
     image = Image.open("map1.png")
     pixels = image.load()
     width, height = image.size
-    maze = [[0 if pixels[i, j] == (229, 229, 229, 255) else 1 for j in range(height)] for i in range(width)]
+    matrix = [[pixels[j, i] for j in range(width)] for i in range(height)]
+    maze = [[0 if matrix[i][j] == (229, 229, 229, 255) \
+             else 1 for j in range(width)] for i in range(height)]
+    
+    start = (height//9 + 60, width//2 - 155)# (height//9, width//4) #
+    end = (height - 28, width//2 - 340)#(height//2 +170, width//2 + 125) #
 
-    # Initialize Pygame
-    pygame.init()
-    screen = pygame.display.set_mode((width, height))
-    pygame.display.set_caption("Pathfinding Demo")
+    #maze = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    #        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    #        [0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+    #        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    #]
+    #start = (9, 9)
+    #end = (0, 0)
+#
+    astar = AStar(maze)
+    #dijkstra = Dijkstra(maze)
+    jps = JPS(maze)
 
-    # Initialize variables
-    start = None
-    end = None
-    algorithm = None
+    start_time = time()
+    path = jps.search(start, end)
+    end_time = time()
+    print(f"JPS: {(end_time - start_time)*1000:.2f} ms ({path[1]} operations)")
+    
 
-    # Define the colors used for the path and start/end points
-    PATH_COLOR = (255, 0, 0)
-    START_COLOR = (0, 255, 0)
-    END_COLOR = (0, 0, 255)
+    start_time = time()
+    path = astar.a_star(start, end)
+    end_time = time()
+    opr = astar.operations
+    print(f"A*: {(end_time - start_time)*1000:.2f} ms ({opr} operations)")
+    
 
-    # Define the font used for displaying text
-    font = pygame.font.Font(None, 30)
-
-    # Create the pathfinding objects
-    dijkstra = Dijkstra(maze)
-    a_star = AStar(maze)
-
-    # Define the main game loop
-    running = True
-    while running:
-        # Handle events
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                # Handle mouse clicks
-                if start is None:
-                    start = event.pos
-                elif end is None:
-                    end = event.pos
-                else:
-                    start = None
-                    end = None
-            elif event.type == pygame.KEYDOWN:
-                # Handle key presses
-                if event.key == pygame.K_d:
-                    algorithm = dijkstra
-                elif event.key == pygame.K_a:
-                    algorithm = a_star
-                elif event.key == pygame.K_j:
-                    algorithm = jps
-
-        # Draw the maze
-        for i in range(width):
-            for j in range(height):
-                color = (255, 255, 255) if maze[i][j] == 0 else (0, 0, 0)
-                pygame.draw.rect(screen, color, (i, j, 1, 1))
-
-        # Draw the start and end points
-        if start is not None:
-            pygame.draw.circle(screen, START_COLOR, start, 5)
-        if end is not None:
-            pygame.draw.circle(screen, END_COLOR, end, 5)
-
-        # Find and draw the path
-        if start is not None and end is not None and algorithm is not None:
-            start_time = time()
-            if algorithm == jps:
-                operations, path = algorithm(start, end, maze)
-            if algorithm == dijkstra:
-                path = algorithm.dijkstra(start, end)
-                operations = algorithm.operations
-            if algorithm == a_star:
-                path = algorithm.a_star(start, end)
-                operations = algorithm.operations
-            end_time = time()
-
-            if path is not None:
-                path = [(y, x) for x, y in path]
-                draw = ImageDraw.Draw(image)
-                draw = draw.line(path, fill=PATH_COLOR, width=2)
-                del draw
-                image.save("path.png")
-
-                for i, j in path:
-                    pygame.draw.rect(screen, PATH_COLOR, (j, i, 1, 1)) 
-
-            # Display the number of operations and the time taken
-            operations_text = font.render("Operations: " + str(operations), True, (255, 255, 255))
-            time_text = font.render("Time: " + str(round(end_time - start_time, 2)) + "s", True, (255, 255, 255))
-
-            screen.blit(operations_text, (0, 0))
-            screen.blit(time_text, (0, 30))
-
-
-
-
-        # Update the display
-        pygame.display.update()
-
-    # Quit the game
-    pygame.quit()
 
 if __name__ == "__main__":
     main()
