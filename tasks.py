a2cdd69b6a5dd0413446a9a7929873fc5b1a80b0
Tuomas Huhtala <tuomas.huhtala@icloud.com>
2023-05-09 13:30:08 +0300

    first release

--- tasks.py
+++ tasks.py
@@ -2,23 +2,23 @@ from invoke import task
 
 @task
 def start(ctx):
-    ctx.run("python3 src/main.py", pty=True)
+    ctx.run("python3 pathfinding/src/main.py", pty=True)
 
 @task
 def test(ctx):
-    ctx.run("pytest ./src", pty=True)
+    ctx.run("pytest ./pathfinding//src", pty=True)
 
 @task
 def lint(ctx):
-    ctx.run("pylint src", pty=True)
+    ctx.run("pylint ./pathfinding/src", pty=True)
 
 @task
 def coverage(ctx):
-    ctx.run("coverage run --branch -m pytest ./src", pty=True)
+    ctx.run("coverage run --branch -m pytest ./pathfinding//src", pty=True)
     ctx.run("coverage report -m", pty=True)
     ctx.run("coverage html", pty=True)
 
 @task
 def clean(ctx):
-    ctx.run("autopep8 --in-place --aggressive ./src/*.py", pty=True)
-    ctx.run("autopep8 --in-place --aggressive ./src/**/*.py", pty=True)
+    ctx.run("autopep8 --in-place --aggressive ./pathfinding/src/*.py", pty=True)
+    ctx.run("autopep8 --in-place --aggressive ./pathfinding/src/**/*.py", pty=True)
